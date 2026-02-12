6434a23c3745e996b97f64f99124704eb92dbbee
Dawid Chaber <dcprojektapp@gmail.com>
2026-02-12 12:17:48 +0100

    test

--- config/db.php
+++ config/db.php
@@ -2,22 +2,22 @@
 // config/db.php
 
 $host = 'localhost';
-$db   = 'szkola_usterki';
+$db = 'szkola_usterki';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
-
+// test
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+     PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 try {
      $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
      // W produkcji nie należy wyświetlać szczegółów błędu użytkownikowi
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+     throw new \PDOException($e->getMessage(), (int) $e->getCode());
 }
 ?>
